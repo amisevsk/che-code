5235ce1f266867cb0509ca533916f470e5092912
Angel Misevski <amisevsk@redhat.com>
2023-02-16 17:58:48 -0500

    Allow using different shell by setting $SHELL environment variable
    Update entrypoint-volume.sh to respect $SHELL env var, if set, instead
    of hard-coding /bin/bash as the login shell in /etc/passwd. If $SHELL is
    unset, the default /bin/bash is used.
    
    Signed-off-by: Angel Misevski <amisevsk@redhat.com>

--- build/scripts/entrypoint-volume.sh
+++ build/scripts/entrypoint-volume.sh
@@ -14,7 +14,7 @@
 # Boilerplate code for arbitrary user support
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:${SHELL:-/bin/bash}" >> /etc/passwd
     echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
   fi
 fi
